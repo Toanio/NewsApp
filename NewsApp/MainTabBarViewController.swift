aba10bfb96bec2efe59f8d2297702f98a0d8c6af
Toan <chan.misha95@mail.ru>
2022-10-27 17:53:20 +0300

    - Добавил Tabbar и View

---
+++ NewsApp/MainTabBarViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainTabBarViewController.swift
+//  NewsApp
+//
+//  Created by c.toan on 27.10.2022.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: SearchViewController())
+        let vc3 = UINavigationController(rootViewController: NotificationViewController())
+        let vc4 = UINavigationController(rootViewController: StatsViewController())
+        
+        vc1.tabBarItem.image = UIImage(named: "homeButton")
+        vc2.tabBarItem.image = UIImage(named: "searchButton")
+        vc3.tabBarItem.image = UIImage(named: "notificationButton")
+        vc4.tabBarItem.image = UIImage(named: "statsButton")
+        
+        tabBar.tintColor = UIColor(red: 0/255, green: 118/255, blue: 162/255, alpha: 100)
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+    }
+    
+
+
+}
---
+++ NewsApp/StatsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StatsViewController.swift
+//  NewsApp
+//
+//  Created by c.toan on 27.10.2022.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
